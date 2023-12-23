a1bcf48fc2b9e561d8a2a2aac5da90aea8ca3870
Matt <21345628+Karstagg@users.noreply.github.com>
2023-12-23 20:24:46 +0900

    Initial Commit

---
+++ Swift UI Challenge/Swift_UI_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_ChallengeApp.swift
+//  Swift UI Challenge
+//
+//  Created by Matthew Fisher on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
