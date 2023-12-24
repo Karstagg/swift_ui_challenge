33bcd73e99c40f6380564cb78d4d1b318c0149e9
Matt <21345628+Karstagg@users.noreply.github.com>
2023-12-24 10:39:31 +0900

    Init

---
+++ Swift UI Challenge/HelloSpeakBuddy.swift
@@ -0,0 +1,22 @@
+//
+//  HelloSpeakBuddy.swift
+//  Swift UI Challenge
+//
+//  Created by Matthew Fisher on 2023/12/24.
+//
+
+import SwiftUI
+
+struct HelloSpeakBuddy: View {
+    var body: some View {
+        VStack {
+            Text("Hello SpeakBUDDY")
+        }
+        .padding()
+        .background()
+    }
+}
+
+#Preview {
+    HelloSpeakBuddy()
+}
