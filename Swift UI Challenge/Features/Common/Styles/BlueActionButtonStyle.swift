79d1082e9f7ff11b65ac3a36b80816af2de3ac45
Matt <21345628+Karstagg@users.noreply.github.com>
2023-12-27 17:52:29 +0900

    Refactor: fix action button capsule style

--- Swift UI Challenge/Features/Common/Styles/BlueActionButtonStyle.swift
+++ Swift UI Challenge/Features/Common/Styles/BlueActionButtonStyle.swift
@@ -10,15 +10,14 @@ import SwiftUI
 
 struct BlueActionButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
-        HStack{
-            configuration.label
-                .padding()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .foregroundColor(.white)
-                .background(.blue)
-                .border(.white)
-                .clipShape(Capsule())
-        }
+        
+        configuration.label
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .font(.title3)
+            .foregroundColor(.white)
+            .background(.actionButtonBlue)
+            .clipShape(Capsule(style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
     }
 }
 
